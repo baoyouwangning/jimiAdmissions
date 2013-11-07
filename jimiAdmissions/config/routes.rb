a12330ad2c22ac5a87978a82acedd70bdccbfa4a
= <=>
2013-11-07 21:21:27 +0800

    root to contents/1/show

--- jimiAdmissions/config/routes.rb
+++ jimiAdmissions/config/routes.rb
@@ -3,6 +3,9 @@ JimiAdmissions::Application.routes.draw do
 
   resources :students
 
+
+  root to: "contents#show"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
