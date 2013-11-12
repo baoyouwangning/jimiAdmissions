f9dc3c6b396387f2c8adbd7c6e7cc1d4bc107426
= <=>
2013-11-12 18:48:52 +0800

    creat form style

---
+++ jimiAdmissions/app/assets/javascripts/studentCreate.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: wangning
+ * Date: 13-11-12
+ * Time: 下午6:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+function studentCreate($scope) {
+    $scope.createButtonStyle = "grayButton";
+    $("#stu_submit_button").click(function () {
+        alert("hell");
+    })
+}
