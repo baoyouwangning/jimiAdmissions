7d9d47a3fd5b34cdeca49c06d0f1ee9804693144
= <=>
2013-11-21 10:42:17 +0800

    start user verification

--- jimiAdmissions/app/controllers/contents_controller.rb
+++ jimiAdmissions/app/controllers/contents_controller.rb
@@ -57,11 +57,11 @@ class ContentsController < ApplicationController
   # DELETE /contents/1
   # DELETE /contents/1.json
   def destroy
-    @content.destroy
-    respond_to do |format|
-      format.html { redirect_to contents_url }
-      format.json { head :no_content }
-    end
+    #@content.destroy
+    #respond_to do |format|
+    #  format.html { redirect_to contents_url }
+    #  format.json { head :no_content }
+    #end
   end
 
   private
