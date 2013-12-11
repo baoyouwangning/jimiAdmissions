b3c87db72762df9240c4683041af0436dcb86aeb
= <=>
2013-12-11 21:28:11 +0800

    refresh

--- jimiAdmissions/app/models/student.rb
+++
@@ -1,9 +0,0 @@
-class Student < ActiveRecord::Base
-  validates :student_name, presence: true,
-            length: {minimum: 1 }
-  validates :obtain_courses, presence: true,
-            length: { minimum: 2 }
-  validates :contact_information, presence: true,
-            length: { minimum: 1}
-
-end
